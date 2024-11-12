77aa321f4563e3a7118ad2f892893fab03cdc120
Andrey <andreykrmr@gmail.com>
2024-11-12 14:40:16 +0300

    Add files via upload

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Changelog for CRT
+
+## [0.0.1] - 2024-11-12
+### Initial Commit
---
+++ CRT.h
@@ -0,0 +1,13 @@
+#ifndef CRT_H
+#define CRT_H
+
+#include <Arduino.h>
+
+
+byte getBrightCRT(byte val);
+
+
+
+
+
+#endif
